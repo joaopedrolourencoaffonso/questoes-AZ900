0bb37802f25795eb9d71d496f0ba6d30b50d01d0
João Pedro Lourenço Affonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2025-11-27 17:31:24 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def hello_moon():
+    return render_template('index.html')
+
+@app.route('/data')
+def json_data():
+    with open('static\\questoes.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/config')
+def config_data():
+    with open('static\\config.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
